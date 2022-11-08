196df40e40f11901dd0348c7f1f0e345691232b6
althebeast <88689564+althebeast@users.noreply.github.com>
2022-11-08 15:38:38 -0400

    Done Files Of The Website

---
+++ UnluKorsanlar/ali-reis.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <!DOCTYPE html>
+    <html lang="tr">
+      <head>
+        <meta charset="utf-8">
+        <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+        <title>Ünlü Korsanlar ve Korsanların Hikayeleri Hepsi Burada</title>
+        <meta name="description" content="Bu sitede ünlü korsanlar ve bu korsanların
+        hikayelerini, başlarından neler geçtiğini, en ünlü korsan savaşlarını ve
+        daha bir çok bilgeye sitemizden ulaşabilirsiniz.">
+        <meta name="keywords" content="korsanlar, ünlü korsanlar, korsan savaşları, türk korsanları,
+        büyük savaşlar, deniz savaşları, korsan hikayeleri, ali-reis,barbaros hayrettin paşa,henry morgan, jack rachkam, kara sakal, kaptan kara sakal, black beard, navarin savaşı, nazik bonnet, piri reis, oruç reis, rodos fethi, turgut reis">
+        <link rel="stylesheet" type="text/css" href="pstyle.css">
+          <script src="https://kit.fontawesome.com/44c7da2e64.js" crossorigin="anonymous"></script>
+      </head>
+      <body id="goup">
+       
+        <h1 title="Ana Başlık" id="anabaslik" ></h1>
+        <div class="menu-bar">
+          <ul>
+            <li><a href="index.html"><i class="fa-solid fa-house"></i> Anasayfa</a></li>
+            <li><i class="fa-solid fa-skull-crossbones"></i> Türk Korsanlarımız
+              <div class="sub-menu-2">
+                <ul>
+                    <li><a href="bhp.html">Barbaros Hayreddin Paşa</a></li>
+                    <li><a href="turgut-reis.html">Turgut Reis</a></li>
+                    <li><a href="piri-reis.html">Piri Reis</a></li>
+                    <li><a href="ali-reis.html">Seydi Ali Reis</a></li>
+                    <li><a href="oruc-reis.html">Oruç Reis</a></li>
+                  </ul>
+              </li>
+              <li><i class="fa-solid fa-skull-crossbones"></i> Yabancı Korsanlar</a>
+              <div class="sub-menu-1">
+                <ul>
+                  <li><a href="kara-sakal.html">Kara Sakal</a></li>
+                  <li><a href="jack-racham.html">John Rackam</a></li>
+                  <li><a href="nazik-bonnet.html">Nazik Bonnet</a></li>
+                  <li><a href="henry-morgan.html">Henry Morgan</a></li>
+                  <li><a href="kara-sam.html">Kara Sam</a></li>
+                </ul>
+            </li>
+            <li><i class="fa-solid fa-anchor"></i> Korsan Deniz Savaşları
+                <div class="sub-menu-3">
+                    <ul>
+                      <li><a href="preveze.html">Preveze Deniz Savaşı</a></li>
+                      <li><a href="navarin.html">Navarin Savaşı (Fâciası)</a></li>
+                      <li><a href="nazik-bonnet.html">Rodos'un Fethi</a></li>
+                    </ul>
+            </li>
+            <li><a href="olumler-tablo.html"><i class="fa-solid fa-skull"></i> Korsanların Ölümleri</a></li>
+          </ul>
+      </div>  
+        <hr>
+        <h2 id="konubasligi">Denizcilik Derecesi: <i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i><i class="fa-solid fa-star"></i> <br>Seydi Ali Reis</h1><br><br>
+        <center><img src="images/alireis.jpeg" width="500" alt="Seydi Ali Reis" title="Barbaros Hayrettin Paşa"></center><br>
+        <p id="p1">Seydi Ali Reis Galata'da doğdu. İstanbul'un fethinden sonra Sinop'a yerleşen denizci bir ailenin oğluydu. Dedesi ve babası tersane kethüdasıydı. 1522 yılında Rodos seferine katılan Seydi Ali Reis, Barbaros Hayreddin Paşa'nın emrinde bir çok deniz seferine çıktı ve Batı Akdeniz bölgesini çok iyi öğrendi. <br><br>
+
+ 
+
+            Preveze Savaşı'ndan sonra adı daha çok duyulmaya başladı. Trablusgarp'ın fethi ile biten harekatta Kaptan-ı Derya Sinan Paşa ve Turgut Reis emrinde çalıştı. Basra'da, bir Osmanlı donanmasını Süveyş'e getirmek için, 1553 yılında Hint Kaptanı tayin edildi. Seydi Ali Reis 34 parçalık Portekiz donanması ile Güney Arabistan sahillerinde karşılaştı. Fırtınaya ve şiddetli düşman taarruzuna rağmen Demen kalesi önüne gelebildi. Burada karaya oturan üç gemiden sonra, elinde kalan altı gemiyle birlikte Güceret'in başkenti Ahmedabat'a gitti. Süveyş'i geçemeyeceğini anlayan Seydi Ali Reis gemileri ve mühimmatı satarak parasını İstanbul'a gönderdi ve üç yıl Osmanlı ülkesi dışında yaşadı. <br><br>
+            
+             
+            
+            1557 yılında İstanbul'a döndüğünde, mahvolmuş bir donanmanın sorumlusu olmakla beraber, başına gelen olağanüstü olaylar yüzünden suçlu görülmedi. Önce müteferrika yapıldı. Ardından Diyarbakır tımar defterdarı tayin edildi. Bir süre Şehzade Selim'in hizmetinde çalıştı. Galata Hassa gemi reislerinden biri oldu. 1562 yılında İstanbul'da öldü.
+            
+            </p>
+
+    
+            
+            
+            <div class="wrapper">
+              <form action="" method="post" class="form">
+                <input type="text" class="name" name="name" placeholder="İsminiz">
+                <br>
+                <textarea name="message" class="message" cols="30" rows="10" placeholder="Mesajınız"></textarea>
+                <br>
+                <button type="submit" class="btn" name="post_comment">Yorumunuzu Paylaşın</button>
+              </form>
+            </div>
+            
+            <div class="content">
+              <h3>Alperen Sarışan</h3>
+              <p>Bu benim ilk yorumum</p>
+            </div>
+
+<footer>
+  <div class="footer-content">
+    <h3>Ünlü Korsanlar</h3>
+    <p>Ünlü Korsanlar sitesi 2022 yılında Alperen Sarışan tarafından kurulmuştur.</p>
+    <ul class="socials">
+      <li><a href="www.facebook.com/alperensarisan">
+        <img src="images/facebook.png" alt="Facebook" style="width:42px;height:42px;"> 
+      </a></li>
+      <li><a href="www.instagram.com/alperensarisan">
+        <img src="images/instagram.png" alt="Facebook" style="width:42px;height:42px;"> 
+      </a></li></i></a></li>
+    </ul>
+  </div>
+  <div class="footer-bottom">
+    <p>copyright &copy;2022 ünlüKorsanlar. designed by <span>Alperen Sarışan</span></p>
+  </div>
+</footer>
+  </body>
+</html>
